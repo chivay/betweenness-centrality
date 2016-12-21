8ebcdc05cbccd7d923d902d808ae99b190235fd3
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2016-12-21 23:03:05 +0100

    Implement basic graph functions

--- include/graph.h
+++ include/graph.h
@@ -2,21 +2,28 @@
 #define GRAPH_H
 
 #include <unordered_map>
+#include <unordered_set>
 
 template<typename T>
 class Graph {
     
     struct Vertex {
         T id;
+        std::unordered_set<Vertex *> adjacent;
+
+        bool has_neighbor(Vertex *v) {
+            return adjacent.find(v) != adjacent.end();
+        }
     };
 
-    std::unordered_map<T, Vertex*> vertices;
+    std::unordered_map<T, Vertex*> vertices_;
+
+    Vertex* get_vertex(T a);
 
 public:
     void connect(T a, T b);
-
+    bool edge_exists(T a, T b);
 };
-
 
 template class Graph<int>;
 
--- src/graph.cc
+++ src/graph.cc
@@ -1,6 +1,29 @@
 #include "graph.h"
 
 template<typename T>
-void Graph<T>::connect(T a, T b) {
+typename Graph<T>::Vertex* Graph<T>::get_vertex(T a)
+{
+    auto it = vertices_.find(a);
+    if (it == vertices_.end())
+        return nullptr;
+    else
+        return it->second;
 }
 
+template<typename T>
+bool Graph<T>::edge_exists(T a, T b)
+{
+    Vertex* a_ptr = get_vertex(a);
+    Vertex* b_ptr = get_vertex(b);
+
+    if (a_ptr == nullptr)
+        return false;
+
+    return a_ptr->has_neighbor(b_ptr);
+}
+
+template<typename T>
+void Graph<T>::connect(T a, T b)
+{
+
+}
