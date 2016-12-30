06ae13402ba050c1f1d6ee04006e22345655526d
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2016-12-30 14:44:40 +0100

    Reformatting

--- include/graph.h
+++ include/graph.h
@@ -22,9 +22,10 @@ private:
         T id_;
         std::unordered_set<Vertex*> adjacent_;
         std::unordered_set<T> adjacent_ids_;
+
         Vertex(const T &id) : id_(id) {}
 
-        bool has_neighbor(Vertex *v) {
+        bool has_neighbor(Vertex *v) const {
             return adjacent_.find(v) != adjacent_.end();
         }
 
